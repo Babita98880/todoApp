230bb632fdc06cb0b9f643deed785c97f5b7da43
Babita <a00280963@mycambrian.ca>
2023-07-30 22:04:29 -0400

    initial commit

---
+++ Models/TodoItem.cs
@@ -0,0 +1,7 @@
+public class TodoItem
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsDone { get; set; }
+}
